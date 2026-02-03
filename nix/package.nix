5ca7c92fb74f897d8fd981c30ec1660b71f1fd4a
Ilham Putra Husada <106536348+hambosto@users.noreply.github.com>
2026-02-03 12:30:51 +0700

    build: update dependencies and optimize release profile
    Adjusted feature sets for several crates, including switching Tokio to 'full'
    and reducing Clap features. Refined release build profile for better
    optimization and smaller binaries. Updated Cargo.lock and Nix configuration.

--- nix/package.nix
+++ nix/package.nix
@@ -1,40 +1,28 @@
 {
   lib,
   rustPlatform,
-  stdenvAdapters,
-  clangStdenv,
 }:
-(rustPlatform.buildRustPackage.override {
-  stdenv = stdenvAdapters.useMoldLinker clangStdenv;
+rustPlatform.buildRustPackage (final: {
+  pname = "sweetbyte-rs";
+  version = "v26.1.0";
+
+  src = lib.fileset.toSource {
+    root = ../.;
+    fileset = lib.fileset.unions [
+      ../assets
+      ../src
+      ../Cargo.lock
+      ../Cargo.toml
+    ];
+  };
+
+  cargoLock.lockFile = ../Cargo.lock;
+
+  meta = {
+    description = "A very small, very simple, yet very secure encryption tool written in rust.";
+    homepage = "https://github.com/hambosto/sweetbyte-rs";
+    license = lib.licenses.mit;
+    mainProgram = "sweetbyte-rs";
+    platforms = lib.platforms.unix;
+  };
 })
-  (
-    final:
-    let
-      inherit (lib.fileset) toSource unions;
-      inherit (lib) licenses platforms;
-    in
-    {
-      pname = "sweetbyte-rs";
-      version = "v26.1.0";
-
-      src = toSource {
-        root = ../.;
-        fileset = unions [
-          ../assets
-          ../src
-          ../Cargo.lock
-          ../Cargo.toml
-        ];
-      };
-
-      cargoLock.lockFile = ../Cargo.lock;
-
-      meta = {
-        description = "A very small, very simple, yet very secure encryption tool written in rust.";
-        homepage = "https://github.com/hambosto/sweetbyte-rs";
-        license = licenses.mit;
-        mainProgram = "sweetbyte-rs";
-        platforms = platforms.unix;
-      };
-    }
-  )
