e48af296b0e62d1574ba40fd966dbc9f78f5384f
Ilham Putra Husada <106536348+hambosto@users.noreply.github.com>
2026-02-08 17:50:25 +0700

    build(flake): pin rust-overlay nixpkgs input

--- flake.nix
+++ flake.nix
@@ -4,7 +4,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
