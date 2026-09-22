0c593a58a7cd1cdf1f5d830d447088bb216e843b
Ilham Putra Husada <106536348+hambosto@users.noreply.github.com>
2026-09-23 01:06:50 +0700

    refactor(ui): disable progress bar in tests

--- src/ui/progress.rs
+++ src/ui/progress.rs
@@ -3,25 +3,32 @@ use cliclack::ProgressBar;
 const PROGRESS_TEMPLATE: &str = "{msg} [{bar:40.cyan/blue}] {bytes}/{total_bytes} ({bytes_per_sec}, {eta})";
 
 pub(crate) struct Progress {
-    progress_bar: ProgressBar,
+    progress_bar: Option<ProgressBar>,
 }
 
 impl Progress {
     pub(crate) fn new(total: u64, label: impl Into<String>) -> Self {
+        if cfg!(test) {
+            return Self { progress_bar: None };
+        }
+
         let progress_bar = cliclack::progress_bar(total).with_template(PROGRESS_TEMPLATE);
         progress_bar.start(label.into());
 
-        Self { progress_bar }
+        Self { progress_bar: Some(progress_bar) }
     }
 
     pub(crate) fn increment(&self, delta: u64) {
-        self.progress_bar.inc(delta);
+        if let Some(progress_bar) = &self.progress_bar {
+            progress_bar.inc(delta);
+        }
     }
 }
 
 impl Drop for Progress {
     fn drop(&mut self) {
-        #[cfg(not(debug_assertions))]
-        self.progress_bar.stop("Done");
+        if let Some(progress_bar) = &self.progress_bar {
+            progress_bar.stop("Done");
+        }
     }
 }
